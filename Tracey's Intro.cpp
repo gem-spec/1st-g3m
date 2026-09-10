225aa9ad7a14f2cb8bf9737f0219308cc3155ab9
gem-spec <gemenettracet@gmail.com>
2026-09-10 17:41:31 +0300

    Add files via upload

---
+++ Tracey's Intro.cpp
@@ -0,0 +1,24 @@
+/*
+Author:Gemenet
+Reg Number:BCS-05-0533/2026
+Description:Units offered this semester
+Date:9th September 2026
+Version 1
+*/
+
+#include<stdio.h> //scanf(),printf()
+
+int main(){
+	printf("Units offered this semester:\n");
+	
+	
+	printf("1.Introduction to programming\n");
+	printf("2.Data structures\n");
+	printf("3.Computer networks\n");
+	printf("4.Database systems\n");
+	printf("5.Software engineering\n");
+ 
+	return 0;//execution succesfull
+	
+	}
+	
