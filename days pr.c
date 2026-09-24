8b782c7fe6a50d958670e5c4235b9cbbb254e01a
gem-spec <gemenettracet@gmail.com>
2026-09-24 15:08:21 +0300

    Library fine calculation

---
+++ days pr.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main(){
+	int BookID,DueDate,ReturnDate,daysOverdue,fineRate,fineAmount;
+	
+	printf("Enter the BookID: \t");
+	scanf("%d",&BookID);
+	
+	printf("Enter the DueDate: \t");
+	scanf("%d",&DueDate);
+	
+	printf("Enter the ReturnDate: \t");
+	scanf("%d",&ReturnDate);
+	
+	printf("Enter the fineAmount: \t");
+	scanf("%d",&fineAmount);
+	
+	daysOverdue = ReturnDate - DueDate;
+	
+	printf("Days Overdue = %d.\n",daysOverdue);
+	
+	if(daysOverdue>=15){
+		fineRate=100;
+		fineAmount=daysOverdue*fineRate;
+	}
+		else if(daysOverdue>=14 && daysOverdue<=8){
+			fineRate=50;
+			fineAmount=daysOverdue*fineRate;
+	
+	}
+	
+	else{
+		fineRate=20;
+		fineAmount=daysOverdue*fineRate;
+	
+	}
+		
+	printf("Enter the user details: \n");
+	printf("BookID=%d. \n",BookID);
+	printf("DueDate=%d. \n",DueDate);
+	printf("ReturnDate=%d. \n",ReturnDate);
+	printf("daysOverdue=%d. \n",daysOverdue);
+	printf("fineRate=%d. \n",fineRate);
+	printf("fineAmount=%d. \n",fineAmount);
+
+		
+		return 0; 
+	
+}
